a6642be6074a09aa617e9cf23a5776a7aab05d42
Abhishek Madhu <21152008+abhishekmadhu@users.noreply.github.com>
2018-06-21 13:04:08 +0530

    DAY 11
    INPUT : DONE
    DBstore : DONE
    VERIFICATION : DONE
    DISPLAY : DONE

---
+++ src/app.py
@@ -0,0 +1,91 @@
+import datetime
+import uuid
+
+from src.common.database import Database
+from src.models.student_data import Students
+from src.models.user import User
+from flask import Flask, render_template, request, session, make_response
+
+__author__ = "abhishekmadhu"
+
+app = Flask(__name__)  # '__main__'
+app.secret_key = "abhi"
+
+
+@app.route('/')  # www.myweb.com/api/login
+def home_template():
+    return render_template('home.html')
+
+
+@app.route('/login')  # www.myweb.com/api/login
+def login_template():
+    return render_template('login.html')
+    # return "Hello World"
+
+
+@app.route('/register')  # www.myweb.com/api/register
+def register_template():
+    return render_template('register.html')
+
+
+# initialize the database b4 any request, only once per session
+@app.before_first_request
+def initialize_database():
+    Database.initialize()  # remove this after clearing the session
+
+
+@app.route('/auth/login', methods=['POST'])
+def login_user():                               # Refactoring DONE
+    email = request.form['email']
+    password = request.form['password']
+
+    if Students.is_login_valid(email, password):        #is True
+        Students.login(email)
+        session['email'] = email
+    else:
+        session['email'] = "no email"
+        return "INVALID USER, PLEASE CHECK YOUR CREDENTIALS, OR REGISTER (if you have not yet)"
+
+    student = Students.from_mongo_by_email(email=email)
+
+    return render_template("student_data.html", email=session['email'], student=student)
+    # return session['email']
+
+
+@app.route('/auth/register', methods=['POST'])      # Refactoring DONE
+def register_user():
+    email = request.form['email']
+    password = request.form['password']
+    institute = request.form['institute']
+    guardian_name = request.form['guardian_name']
+    student_name = request.form['student_name']
+    course = request.form['course']
+    created_date = datetime.datetime.utcnow()
+    _id = uuid.uuid4().hex
+
+    a = Students.register(email=email, password=password, institute=institute, guardian_name=guardian_name,
+                          student_name=student_name, created_date=created_date, _id=_id, course=course)
+
+    # #########remove block if does not work
+    # also return render_template("profile.html", email=session['email'])
+    # user = User.get_by_email(email=email)
+    # blog = Blog.find_by_author_id(user._id)
+    # ##########################################
+    student = Students.from_mongo_by_email(session['email'])
+
+    if a == 1:
+        return render_template("profile.html", email=session['email'], student=student)
+    elif a == 0:
+        return "You are already registered, please log in"
+    elif a == 100:
+        return "Server is unresponsive, contact (+91)7063375758 immediately."
+
+
+@app.route('/student_details/<string:_id>', methods=['GET'])
+def show_details_for_new_students(_id):
+    student = Students.from_mongo_by_id(_id=_id)
+    return render_template("student_data.html", email=session['email'], student=student)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ src/models/student_data.py
@@ -0,0 +1,106 @@
+from flask import session
+
+__author__ = "abhishekmadhu"
+
+import uuid
+from src.common.database import Database
+import datetime
+__author__ = "abhishekmadhu"
+
+
+class Students(object):
+    def __init__(self, email, password, institute, guardian_name, student_name, course,
+                 created_date, _id=None):
+        self.email = email
+        self.password = password
+        self.institute = institute
+        self.guardian_name = guardian_name
+        self.student_name = student_name
+        self.course = course
+        self._id = _id
+        self.created_date = created_date
+
+    def save_to_mongo(self):
+        Database.insert(collection='students', data=self.json())
+
+    def json(self):
+        return {
+            '_id': self._id,
+            'email': self.email,
+            'password': self.password,
+            'student_name': self.student_name,
+            'guardian_name': self.guardian_name,
+            'institute': self.institute,
+            'created_date': self.created_date,
+            'course': self.course
+        }
+
+    @classmethod
+    def from_mongo_by_id(cls, _id):
+        # Post.from_mongo('123')
+        student_data = Database.find_one(collection='students',
+                                         query={'_id': _id})
+        return cls(**student_data)
+
+    @classmethod
+    def from_mongo_by_email(cls, email):
+        data = Database.find_one(collection='students',
+                                 query={'email': email})
+        if data is not None:
+            return cls(**data)
+        return None
+
+    # this is equivalent to the following --
+    # return cls(
+    #                    institute=student_data['institute'],
+    #                    guardian_name=student_data['guardian_name'],
+    #                    student_name=student_data['student_name'],
+    #                    created_date=student_data['created_date'],
+    #                    course=student_data['course'],
+    #                    _id=student_data['_id'])
+
+    # LOGIN RELATED FUNCTIONS   ##############################
+
+    @staticmethod
+    def is_login_valid(email, password):
+        # User.is_login_valid("abhishekmadhu.uem@gmail.com", "1234")
+        student = Students.from_mongo_by_email(email=email)
+        if student is not None:
+            return student.password  # == password
+        return False
+
+    @classmethod
+    def register(cls, email, password, institute, guardian_name, student_name,
+                 course, created_date, _id=None):
+
+        student = cls.from_mongo_by_email(email)
+
+        if student is None:
+            # student does not exist
+            new_student = cls(email, password, institute, guardian_name, student_name,
+                              course, created_date, _id)
+            new_student.save_to_mongo()
+            session['email'] = email
+            return 1
+        else:
+            # user exists
+            return 0
+
+    @staticmethod
+    def login(user_email):
+        # if the login credentials above are valid
+        # login already valid ie: is_login_valid already called
+
+        session['email'] = user_email
+
+    @staticmethod
+    def logout(self):
+        session['email'] = None
+
+    def get_details(self):
+        return Students.from_mongo_by_id(self._id)    # REFACTORED FUNCTION
+
+    # @staticmethod
+    # def using_id(_id):  # blog _id for all posts by one person is same
+    #     return [student for student in Database.find(collection='students',
+    #                                            query={'_id': _id})]

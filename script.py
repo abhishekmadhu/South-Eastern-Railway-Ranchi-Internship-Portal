582da9c63aad67480f247544cab93efcd15f27ac
Abhishek Madhu <abhishek.prince.madhu@gmail.com>
2018-07-19 21:17:57 +0530

    commit+++

--- script.py
+++ script.py
@@ -1,11 +1,16 @@
-from pymongo import MongoClient
+import pymongo
 
-client = MongoClient("mongodb://admin:XOOvxd32349@mongodb16086-hello2.mj.milesweb.cloud:27017")
-db = client.serly
-a = "Hello"
-print(a)
+class Database(object):
+	URI = "mongodb://admin:XOOvxd32349@mongodb16086-hello2.mj.milesweb.cloud:27017"
+	DATABASE = None
 
-try: db.command("serverStatus")
-except Exception as e: print(e)
-else: print("You are connected!")
-client.close()
+	client = pymongo.MongoClient(URI)
+	DATABASE = client['serly']
+
+	a = "Hello"
+	print(a)
+
+	try: DATABASE.command("serverStatus")
+	except Exception as e: print(e)
+	else: print("You are connected!")
+	client.close()
